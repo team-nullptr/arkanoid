9868e50a2a49e18af942e3ff2406ac475b23027e
Pietrek14 <dawidekpe@gmail.com>
2023-01-23 23:50:19 +0100

    window descriptor

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                title: "Arkanoid".to_string(),
+                // qHD
+                width: 960.0,
+                height: 540.0,
+                resizable: false,
+                ..default()
+            },
+            ..default()
+        }))
+        .run();
 }
